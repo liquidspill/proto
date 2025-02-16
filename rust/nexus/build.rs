e6a6c016ec4e3a41c3e102d6a29d3e3fae79d3b8
senyo simpson <senyo@spilled.io>
2025-02-16 17:42:37 +0200

    flatten protobuf definition location

--- rust/nexus/build.rs
+++ rust/nexus/build.rs
@@ -2,9 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .emit_rerun_if_changed(true)
-        .compile_protos(
-            &["../../proto/nexus/v1/nexus.proto"],
-            &["../../proto/nexus/v1"],
-        )?;
+        .compile_protos(&["../../nexus.v1.proto"], &["../.."])?;
     Ok(())
 }
