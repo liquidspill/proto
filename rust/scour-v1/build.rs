e1ff528e7b6d4a7879c16321d068b0200c2a30f1
senyo simpson <senyo@spilled.io>
2025-02-26 09:57:58 +0200

    build server

--- rust/scour-v1/build.rs
+++ rust/scour-v1/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .build_server(false)
+        .build_server(true)
         .emit_rerun_if_changed(true)
         .compile_protos(&["../../scour.v1.proto"], &["../.."])?;
     Ok(())
