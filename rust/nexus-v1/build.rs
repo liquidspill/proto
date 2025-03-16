0c82aa55d60874d69324933bd51d6a86289b8442
senyo simpson <senyo@spilled.io>
2025-03-15 22:29:57 -0400

    point to relevant proto files

--- rust/nexus-v1/build.rs
+++ rust/nexus-v1/build.rs
@@ -2,6 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .emit_rerun_if_changed(true)
-        .compile_protos(&["../../nexus.v1.proto"], &["../.."])?;
+        .compile_protos(&["../../proto/nexus/v1/nexus.proto"], &["../.."])?;
     Ok(())
 }
--- rust/scour-v1/build.rs
+++ rust/scour-v1/build.rs
@@ -2,6 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .emit_rerun_if_changed(true)
-        .compile_protos(&["../../scour.v1.proto"], &["../.."])?;
+        .compile_protos(&["../../proto/scour/v1/scour.proto"], &["../.."])?;
     Ok(())
 }
