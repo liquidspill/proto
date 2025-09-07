2c498a1c44a1e418a7159aea9aacc4d78731b34d
senyo simpson <senyo@spilled.io>
2025-09-07 10:51:31 +0200

    update export names

--- js/index.ts
+++ js/index.ts
@@ -1,2 +1,2 @@
-export * from "./scour/query/v1/query_pb";
-export * from "./nexus/metadata/v1/metadata_pb";
+export * as Query from "./scour/query/v1/query_pb";
+export * as Metadata from "./nexus/metadata/v1/metadata_pb";
