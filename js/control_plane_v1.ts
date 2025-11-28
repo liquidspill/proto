8037458dbb7a3ad9a367f6afd25ae052a1839042
senyo simpson <senyo@spilled.io>
2025-11-28 15:31:00 +0200

    fix imports

---
+++ js/control_plane_v1.ts
@@ -0,0 +1,2 @@
+export * from "./nexus/controlplane/v1/control_plane_pb";
+export * from "./nexus/controlplane/v1/permissions_pb";
--- js/index.ts
+++ js/index.ts
@@ -1,4 +1,2 @@
-import * as ControlPlane from "./nexus/controlplane/v1/control_plane_pb";
-import * as Permissions from "./nexus/controlplane/v1/permissions_pb";
-export const ControlPlaneV1 = { ...ControlPlane, ...Permissions };
+export * as ControlPlaneV1 from "./control_plane_v1";
 export * as CatalogV1 from "./nexus/catalog/v1/catalog_pb";
