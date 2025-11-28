84534ffe1097b90fc9e6f0a4cffd510fff1b278c
senyo simpson <senyo@spilled.io>
2025-11-28 08:14:12 +0200

    update js package

--- js/index.ts
+++ js/index.ts
@@ -1,2 +1,4 @@
-export * as ControlPlane from "./nexus/controlplane/v1/control_plane_pb";
-export * as Catalog from "./nexus/catalog/v1/catalog_pb";
+import * as ControlPlane from "./nexus/controlplane/v1/control_plane_pb";
+import * as Permissions from "./nexus/controlplane/v1/permissions_pb";
+export const ControlPlaneV1 = { ...ControlPlane, ...Permissions };
+export * as CatalogV1 from "./nexus/catalog/v1/catalog_pb";
